debe9a05eda02f677e228fb6e71b840703028c4b
carlo-ddm <carloduminuco90@gmail.com>
2022-04-15 11:09:11 +0200

    correzione punto virgola jsnack1 / inizio jsnack2

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,14 +1,14 @@
 console.log('test');
 
 // Controllo natura dei dati inseriti
-let insertOk = true
+let insertOk = true;
 
 // Chiedo all'utente di inserire il primo numero
 const first_number = parseInt(prompt('inserisci il primo numero'));
 
 if (isNaN(first_number)) {
-  alert('Inserire un dato numerico')
-  insertOk = false
+  alert('Inserire un dato numerico');
+  insertOk = false;
 }
 
 console.log(first_number);
@@ -17,8 +17,8 @@ console.log(first_number);
 const second_number = parseInt(prompt('inserisci il secondo numero'));
 
 if (isNaN(second_number)) {
-  alert('Inserire un dato numerico')
-  insertOk = false
+  alert('Inserire un dato numerico');
+  insertOk = false;
 }
 
 console.log(second_number);
@@ -31,11 +31,11 @@ const result = document.getElementById ('result');
 
 if (insertOk === true){
   if (first_number > second_number){
-    result.innerHTML = `${first_number} > ${second_number} --> il numero con valore più alto è: ${first_number}`
+    result.innerHTML = `${first_number} > ${second_number} --> il numero con valore più alto è: ${first_number}`;
   } else if (first_number < second_number){
-    result.innerHTML = `${second_number} > ${first_number} --> il numero con valore più alto è: ${second_number}`
+    result.innerHTML = `${second_number} > ${first_number} --> il numero con valore più alto è: ${second_number}`;
   } else {
-    result.innerHTML = `${first_number} = ${second_number} --> hai scelto due numeri con lo stesso valore`
+    result.innerHTML = `${first_number} = ${second_number} --> hai scelto due numeri con lo stesso valore`;
   }
 } else {
   message = 'Non hai inserito correttamente i dati richiesti';
---
+++ jsnack2/script.js
@@ -0,0 +1,9 @@
+console.log('test');
+
+// chiedere di inserire la prima parola
+const first_word = prompt('Inserisci la prima parola');
+
+// chiedere di inserire la seconda parola
+const second_word = prompt('Inserisci la seconda parola');
+
+let insertOk = true;
