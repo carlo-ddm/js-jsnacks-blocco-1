c05906466cdcb94927b1445b8e0445a9315f24d3
carlo-ddm <carloduminuco90@gmail.com>
2022-04-16 03:33:23 +0200

    jsnack5

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -15,26 +15,31 @@ let fifth_addend;
 
 while (!ok){
   first_addend = parseInt(prompt ('Inserisci il primo addendo'));
+  console.log('primo addendo: ',first_addend);
   while (isNaN(first_addend)) {
     alert('inserisci un numero');
     first_addend = parseInt(prompt ('Inserisci il primo addendo'));
   }
   second_addend = parseInt(prompt ('inserisci il secondo addendo'));
+  console.log('secondo addendo: ',second_addend);
   while (isNaN(second_addend)) {
     alert('inserisci un numero');
     second_addend = parseInt(prompt ('Inserisci il secondo addendo'));
   }
   third_addend = parseInt(prompt ('Inserisci il terzo addendo'));
+  console.log('terzo addendo: ',third_addend);
   while (isNaN(third_addend)) {
     alert('inserisci un numero');
     third_addend = parseInt(prompt ('Inserisci il terzo addendo'));
   }
   fourth_addend = parseInt(prompt ('Inserisci il quarto addendo'));
+  console.log('quarto addendo: ',fourth_addend);
   while (isNaN(fourth_addend)) {
     alert('inserisci un numero');
     fourth_addend = parseInt(prompt ('Inserisci il quarto addendo'));
   }
   fifth_addend = parseInt(prompt ('Inserisci il quinto addendo'));
+  console.log('quinto addendo: ',fifth_addend);
   while (isNaN(fourth_addend)) {
     alert('inserisci un numero');
     fifth_addend = parseInt(prompt ('Inserisci il quinto addendo'));
---
+++ jsnack5/script.js
@@ -0,0 +1,33 @@
+/*
+ Crea un array vuoto.
+ Chiedi per 6 volte all’utente di inserire un numero,
+ se è dispari inseriscilo nell’array
+*/
+
+console.log('test');
+
+let box = [];
+const limit = 6;
+let number;
+let result;
+
+for (i = 0; i < limit; i ++){
+  number = prompt('Inserisci un numero');
+  while (isNaN(number)){
+    alert('Non hai inserito un numero.')
+    number = prompt('Inserisci un numero');
+  }
+  console.log(number);
+  if (number % 2){
+    box.push(number);
+    console.log(box);
+  }
+}
+result = document.getElementById('print').innerHTML = `Ecco i numeri dispari che hai selezionato: ${box}`;
+
+
+/**
+ Sto dicendo: se il modulo restituisce come resto un numero > 0 la condizione sarà quindi verificata (es. 7 / 2 = 3 col resto di 1, quindi 7 sarà pushato nell'array).
+ Al contrario: se nego questa condizione dirò che non è vero che quella condizione non è vera (es. 8 / 2  = 4, con 0 di resto). La condizione sarà negativa ma essendo negata sarà positiva, quindi il numero 8 verrà pushato)
+ */
+
